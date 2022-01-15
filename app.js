2da31a48f3ead8bf6081c6df4a73080c508e5d34
ThibaudS <thibaud.devpro@gmail.com>
2022-01-15 12:46:53 +0100

    Added Battle logic

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ function shuffleDeck(arr) {
         }
     })
 }
+
 const cardDeckEmpty = []
 const cardDeck = cardDeckEmpty.concat(heartsCard, spadesCard, diamondsCard, clubsCard)
 
@@ -53,11 +54,14 @@ for (let i = 0; i < cardDeck.length; i++) {
         IADeck.push(cardDeck[i])
     }
 }
+
 console.log('taille mon deck: ', playerDeck.length, playerDeck)
 console.log("taille du deck de l'ia: ", IADeck.length, IADeck)
 console.log("taille du deck initial: ", cardDeck.length, cardDeck)
 
 let rewards = []
+let remainingCardsPlayerDeck = true
+let remainingCardsIADeck = true
 
 function startRound(firstCardPlayer, firstCardIA) {
     console.log('DEBUT DU TOUR: ', 'MA FIRST CARD: ', firstCardPlayer, 'FIRST CARD IA: ', firstCardIA)
@@ -79,22 +83,89 @@ function IAWin(IADeck) {
     rewards.splice(0, rewards.length)
 }
 
+function randomizeIndex(deck) {
+    let numberOfCardsAvailable = deck.length
+    return Math.floor(Math.random() * numberOfCardsAvailable)
+}
+
+function verifyStateDeck() {
+    if (playerDeck.length === 0) {
+        remainingCardsPlayerDeck = false
+    } else {
+        remainingCardsPlayerDeck = true
+    }
+
+    if (IADeck.length === 0) {
+        remainingCardsIADeck = false
+    } else if (IADeck.length === 0) {
+        remainingCardsIADeck = true
+    }
+}
+
 function hiddenCard(playerDeck, IADeck) {
-    console.log('=======================================================================================')
-    console.log("Je pose une carte face cachée")
-    rewards.push(playerDeck.shift())
-    console.log(rewards)
-    console.log("L'IA pose une carte face cachée")
-    rewards.push(IADeck.shift())
-    console.log(rewards)
-    return round()
+    verifyStateDeck()
+    if (remainingCardsIADeck && remainingCardsPlayerDeck) {
+
+        console.log('=======================================================================================')
+        console.log("Je pose une carte face cachée")
+        rewards.push(playerDeck.shift())
+        console.log(rewards)
+        console.log("L'IA pose une carte face cachée")
+        rewards.push(IADeck.shift())
+        console.log(rewards)
+
+    } else if (!remainingCardsPlayerDeck) {
+
+        let index = randomizeIndex(IADeck)
+        let cardDrawn = IADeck.splice(index, 1)
+        rewards.concat(cardDrawn)
+        console.log('cardDrawn', cardDrawn)
+        console.log("Je n'ai plus de carte, je pioche dans le deck de l'IA puis je la pose face cachée")
+        console.log("L'IA pose une carte face cachée")
+        rewards.push(IADeck.shift())
+        console.log(rewards)
+
+    } else if (!remainingCardsIADeck) {
+        console.log("Je pose une carte face cachée")
+        rewards.push(playerDeck.shift())
+        console.log(rewards)
+
+        let index = randomizeIndex(playerDeck)
+        let cardDrawn = playerDeck.splice(index, 1)
+        rewards.concat(cardDrawn)
+        console.log('cardDrawn', cardDrawn)
+        console.log("L'IA n'a plus de carte, elle en prend une dans mon Deck puis la pose face cachée")
+
+    }
+    round()
+}
+
+function cardDrawnPlayer() {
+    console.log("Pour ce nouveau tour, je n'ai plus de carte, je pioche dans le deck de l'IA")
+    let index = randomizeIndex(IADeck)
+    console.log('index', index)
+    let cardDrawn = IADeck.splice(index, 1)
+    console.log('cardDrawn', cardDrawn)
+    playerDeck.unshift(cardDrawn[0])
+    console.log('cardDrawn', cardDrawn)
+}
+
+function cardDrawnIADeck() {
+    console.log("Pour ce nouveau tour, L'IA n'a plus de cartes, elle pioche dans mon deck")
+    let index = randomizeIndex(playerDeck)
+    let cardDrawn = playerDeck.splice(index, 1)
+    IADeck.unshift(cardDrawn[0])
+    console.log('cardDrawn', cardDrawn)
 }
 
 function round() {
+    if (playerDeck.length === 0) {
+        cardDrawnPlayer()
+    } else if (IADeck.length === 0) {
+        cardDrawnIADeck()
+    }
 
-    rewards.unshift(playerDeck[0], IADeck[0])
-    playerDeck.shift()
-    IADeck.shift()
+    rewards.unshift(playerDeck.shift(), IADeck.shift())
     console.log('rewards:', rewards)
     startRound(rewards[0], rewards[1])
 
@@ -108,9 +179,7 @@ function round() {
     }
     console.log('Il me reste: ', playerDeck.length, ' cartes')
     console.log('Il reste à l\'IA: ', IADeck.length, ' cartes')
-
 }
-
 
 while (playerDeck.length > 0 && IADeck.length > 0) {
     round()
